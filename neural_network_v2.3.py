219bc8e319949d55b423eaebf844c0bbd9d4e1d7
mario marin <mario.marin.13@sansano.usm.cl>
2018-11-21 20:03:44 -0300

    fixed error

--- neural_network_v2.3.py
+++ neural_network_v2.3.py
@@ -10,7 +10,8 @@ testing_dataset_path = sys.argv[2]
 training_dataset_path = sys.argv[1]
 
 pd.set_option('precision',20)#indica cuandots decimales tiene el flotante
-feature_cols = ["Dur","Proto","SrcAddr_1","SrcAddr_2","SrcAddr_3","SrcAddr_4","Sport","Dir","DstAddr_1","DstAddr_2","DstAddr_3","DstAddr_4","Dport","TotPkts","TotBytes","SrcBytes"]
+feature_cols = ["Dur","Proto","SrcAddr","Sport","Dir","DstAddr","Dport","TotPkts","TotBytes","SrcBytes"]
+#feature_cols = ["Dur","Proto","SrcAddr_1","SrcAddr_2","SrcAddr_3","SrcAddr_4","Sport","Dir","DstAddr_1","DstAddr_2","DstAddr_3","DstAddr_4","Dport","TotPkts","TotBytes","SrcBytes"]
 
 #USAGE: simplemente po todos los dataset con que quieres entrenar y el ultimo dataset sera el de prueba
 
