52dc5dadb9525475fc7a3724a203f41bfd1bd527
Elav95 <34691359+Elav95@users.noreply.github.com>
2018-12-06 11:57:55 -0300

    Add DeltaTime as a new column

---
+++ date-analysis.py
@@ -0,0 +1,15 @@
+import sys
+from datetime import timedelta
+import numpy as np
+import pandas as pd
+
+open_path = pd.read_csv(sys.argv[1], sep=",",header=0)
+output_path = "date-analysis"
+
+open_path.StartTime = pd.to_datetime(open_path.StartTime)
+open_path['DeltaTime'] = open_path.StartTime.diff().shift(0)
+open_path.to_csv(output_path)
+
+
+
+
